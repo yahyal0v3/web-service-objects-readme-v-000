f011a0fa686b04ce27831e0b02b942885c6c1f17
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-13 23:45:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,4 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  def friends(token)
-    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
-      req.params['oauth_token'] = token
-      req.params['v'] = '20160201'
-    end
-    JSON.parse(resp.body)["response"]["friends"]["items"]
-  end
 end
